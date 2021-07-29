6d9569bb7c722b6424c6db29255045fe61e5ce24
SampreethC <87109576+SampreethC@users.noreply.github.com>
2021-07-29 10:02:15 +0530

    Update Exception.js

--- Exception.js
+++ Exception.js
@@ -4,10 +4,8 @@
 //The finally statement lets you execute code, after try and catch, regardless of the result.
 
 
-//try…catch example
-
-
-try{
+    //try…catch example
+    try{
     var a= ["34","32","5","31","24","44","67"]; //a is an array
     console.log(a);    // displays elements of a
     console.log(b); //b is undefined but still trying to fetch its value. Thus catch block will be invoked
@@ -16,13 +14,7 @@ try{
     }
 
 
-
-
-
-
-//throw example with try…catch
-
-
+    //throw example with try…catch
     try {  
         throw "This is the throw keyword"; //user-defined throw statement.  
      }  
@@ -31,14 +23,7 @@ try{
      } 
 
 
-
-
-
-
-
     //try…catch…finally example
-
-    /*
     try{  
         var a=2;  
         if(a==2)  
@@ -50,4 +35,3 @@ try{
         finally{  
         console.log("Value of a is 2 ");  
         }
-        */
